7ff8337eeae7eae369f64af3645a9fd49400a44e
fbioo97 <89308633+fbioo97@users.noreply.github.com>
2022-01-06 14:08:30 -0300

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,14 @@
+module NutriPizza {
+
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires java.compiler;
+    requires java.desktop;
+    requires java.logging;
+    requires java.sql;
+
+    opens Nutri;
+}
